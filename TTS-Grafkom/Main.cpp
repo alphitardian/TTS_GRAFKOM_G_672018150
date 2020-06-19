dd6ab1786d9e1c0b11506c42239e23b5cd65db48
Ardian Alphita <ardianpramudya81@gmail.com>
2020-06-19 14:18:52 +0700

    Logic Update

--- TTS-Grafkom/Main.cpp
+++ TTS-Grafkom/Main.cpp
@@ -6,11 +6,48 @@ using namespace std;
 
 class MyPattern {
 	public:
+		float delta_x = 0.0;
+		float delta_y = 200;
+
+		float details_x1 = 490;
+		float details_x2 = 110;
+		float details_y = 400;
+
+	protected:
+		void drawEclipse(float x_center, float y_center, float x_center2, float y_center2) {
+			/* Filled Eclipse */
+			glColor3f(0.68, 0.32, 0.22);
+			glBegin(GL_POLYGON);
+			for (int i = 0; i < 360; i++) {
+				glVertex2f(cos(i * phi / 180 + 100) * 25 + x_center, sin(i * phi / 180 + 250) * 25 + y_center);
+			}
+			glEnd();
+			glBegin(GL_POLYGON);
+			for (int i = 0; i < 360; i++) {
+				glVertex2f(cos(i * phi / 180 - 100) * 25 + x_center2, sin(i * phi / 180 - 250) * 25 + y_center2);
+			}
+			glEnd();
+			
+			/* Eclipse Line */
+			glColor3f(0.83, 0.82, 0.84);
+			glBegin(GL_LINE_LOOP);
+			for (int i = 0; i < 360; i++) {
+				glVertex2f(cos(i * phi / 180 + 100) * 25 + x_center, sin(i * phi / 180 + 250) * 25 + y_center);
+			}
+			glEnd();
+			glBegin(GL_LINE_LOOP);
+			for (int i = 0; i < 360; i++) {
+				glVertex2f(cos(i * phi / 180 - 100) * 25 + x_center2, sin(i * phi / 180 - 250) * 25 + y_center2);
+			}
+			glEnd();
+		}
+};
+
+class MainBatikPattern : MyPattern {
+	public:
 		void mainPattern(float delta_x, float delta_y) {
-			drawLeftEclipse(25 + delta_x, 275 + delta_y);
-			drawRightEclipse(75 + delta_x, 275 + delta_y);
-			drawRightEclipse(25 + delta_x, 225 + delta_y);	
-			drawLeftEclipse(75 + delta_x, 225 + delta_y);
+			drawEclipse(25 + delta_x, 275 + delta_y, 75 + delta_x, 275 + delta_y);
+			drawEclipse(75 + delta_x, 225 + delta_y, 25 + delta_x, 225 + delta_y);
 		}
 
 		void batikDetails(float point_x1, float point_x2, float point_y1, float point_y2) {
@@ -24,39 +61,6 @@ class MyPattern {
 				glVertex2f(x2, y2);
 				x2 += 10;
 				y2 += 10;
-			}
-			glEnd();
-		}
-
-	private:
-		void drawLeftEclipse(float x_center, float y_center) {
-			glColor3f(0.68, 0.32, 0.22);
-			glBegin(GL_POLYGON);
-			for (int i = 0; i < 360; i++) {
-				glVertex2f(cos(i * phi / 180 + 100) * 25 + x_center, sin(i * phi / 180 + 250) * 25 + y_center);
-			}
-			glEnd();
-
-			glColor3f(0.83, 0.82, 0.84);
-			glBegin(GL_LINE_LOOP);
-			for (int i = 0; i < 360; i++) {
-				glVertex2f(cos(i * phi / 180 + 100) * 25 + x_center, sin(i * phi / 180 + 250) * 25 + y_center);
-			}
-			glEnd();
-		}
-
-		void drawRightEclipse(float x_center, float y_center) {
-			glColor3f(0.68, 0.32, 0.22);
-			glBegin(GL_POLYGON);
-			for (int i = 0; i < 360; i++) {
-				glVertex2f(cos(i * phi / 180 - 100) * 25 + x_center, sin(i * phi / 180 - 250) * 25 + y_center);
-			}
-			glEnd();
-
-			glColor3f(0.83, 0.82, 0.84);
-			glBegin(GL_LINE_LOOP);
-			for (int i = 0; i < 360; i++) {
-				glVertex2f(cos(i * phi / 180 - 100) * 25 + x_center, sin(i * phi / 180 - 250) * 25 + y_center);
 			}
 			glEnd();
 		}
@@ -74,32 +78,28 @@ void myInit() {
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	MyPattern batikPattern;
-
-	float delta_x = 0.0;
-	float delta_y = 200;
+	MyPattern basePattern;
+	MainBatikPattern batikPattern;
 
 	/* Main */
 	for (int i = 0; i < 5; i++) {
 		for (int i = 0; i < 5; i++) {
-			batikPattern.mainPattern(delta_x, delta_y);
-			delta_x += 100;
+			batikPattern.mainPattern(basePattern.delta_x, basePattern.delta_y);
+			basePattern.delta_x += 100;
 		}
-		delta_x = 0.0;
-		delta_y -= 100;
+		basePattern.delta_x = 0.0;
+		basePattern.delta_y -= 100;
 	}
 
 	/* Detail */
-	int detail_x = 490, detail_y = 400;
-	int detail_x2 = 110, detail_y2 = 400;
 	for (int i = 0; i < 5; i++) {
-		batikPattern.batikDetails(10, 0, detail_x, detail_y);
-		detail_x -= 100;
-		detail_y -= 100;
+		batikPattern.batikDetails(10, 0, basePattern.details_x1, basePattern.details_y);
+		basePattern.details_x1 -= 100;
+		basePattern.details_y -= 100;
 
-		batikPattern.batikDetails(detail_x2, detail_y2, 490, 0);
-		detail_x2 += 100;
-		detail_y2 -= 100;
+		batikPattern.batikDetails(basePattern.details_x2, basePattern.details_y, 490, 0);
+		basePattern.details_x2 += 100;
+		basePattern.details_y -= 100;
 	}
 
 	glFlush();
@@ -110,7 +110,7 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Tugas 3 - Grafika Komputer");
+	glutCreateWindow("TTS - Grafika Komputer");
 
 	glutDisplayFunc(display);
 
